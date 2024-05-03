0a7995a2c067563f4e94ce7d1776276908e9fdbf
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-05-04 00:45:20 +0530

    adding more queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,22 @@
 import { cache } from "react";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs/server";
+import { userProgress } from "./schema";
+import { eq } from "drizzle-orm";
+
+
+export const getUserProgress = cache(async()=>{
+    const {userId} = await auth()
+
+    if(!userId){
+        return null
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress)
+    })
+})
+
 
 export const getCourses = cache(async()=>{
     const data = await db.query.courses.findMany()
